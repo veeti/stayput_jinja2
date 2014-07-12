e006875b254e705474a41720113ee6255c23c5b3
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-07-12 23:15:21 +0300

    Update example

--- example/stayput.py
+++ example/stayput.py
@@ -4,4 +4,5 @@ from stayput.jinja2 import Jinja2Templater
 def configure(site):
     site.router = simple_router
     site.templater = Jinja2Templater(site)
+    site.title = 'Stayput Jinja2 Example'
 
